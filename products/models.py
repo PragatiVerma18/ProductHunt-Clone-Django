b971eccd7d5130171c6decfa52996905b5941f74
pragati <itispragativerma@gmail.com>
2019-11-01 19:44:47 +0530

    added product page

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+  title = models.CharField(max_length=255)
+  pub_date = models.DateTimeField()
+  body = models.TextField()
+  url = models.TextField()
+  image = models.ImageField(upload_to= 'images/')
+  icon = models.ImageField(upload_to= 'images/')
+  votes_total = models.IntegerField(default=1)
+  hunter = models.ForeignKey(User, on_delete= models.CASCADE)
+
+  def __str__(self):
+    return self.title
+
+  def summary(self):
+    return self.body[:100]
+
+  def pub_date_pretty(self):
+    return self.pub_date.strftime('%b %e %Y')
--- products/views.py
+++ products/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
   return render(request, 'products/home.html')
+
+@login_required
+def create(request):
+  if request.method == 'POST':
+    if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
+      product = Product()
+      product.title = request.POST['title']
+      product.body = request.POST['body']
+      if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+        product.url = request.POST['url']
+      else:
+        product.url = 'http://'+request.POST['url']
+      product.icon = request.FILES['icon']
+      product.icon = request.FILES['image']
+      product.pub_date = timezone.datetime.now()
+      product.hunter = request.user
+      product.save()
+      return redirect('home')
+
+
+    else:
+      return render(request, 'products/create.html', {'error': 'All fields are required'})
+
+
+  else:
+    return render(request, 'products/create.html')
